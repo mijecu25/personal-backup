824548d5c3b3fca692789759c2e12b7b7385e678
Miguel Velez <miguelvelez@mijecu25.com>
2015-05-26 13:04:39 -0500

    Created Directory and Record

---
+++ src/main/java/com/mijecu25/personal_backup/File/Record.java
@@ -0,0 +1,21 @@
+package com.mijecu25.personal_backup.File;
+
+/**
+ * @author Miguel Velez
+ * 
+ * @version 0.1
+ * 
+ * A record is contained within a directory
+ */
+public class Record {
+
+	/**
+	 * Create are record from the specified {@code pathname}.
+	 * 
+	 * @param pathname the path to a record
+	 */
+	public Record(String pathname) {
+
+	}
+
+}
