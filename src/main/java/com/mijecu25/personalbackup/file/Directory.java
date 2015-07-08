811d77481b3791b0101b7b33546843effae73c93
Miguel Velez <miguelvelez@mijecu25.com>
2015-07-08 16:49:56 -0400

    Listing records from directory

--- src/main/java/com/mijecu25/personalbackup/file/Directory.java
+++ src/main/java/com/mijecu25/personalbackup/file/Directory.java
@@ -1,11 +1,13 @@
 package com.mijecu25.personalbackup.file;
 
+import java.io.File;
+
 /**
  * A directory is a type of record in the file system. It might contain other directories and files.
 
  * @author Miguel Velez
  * 
- * @version 0.1
+ * @version 0.1.1.3
  * 
  */
 public class Directory extends Record {
@@ -17,6 +19,39 @@ public class Directory extends Record {
 	 */
 	public Directory(String pathname) {
 		super(pathname);
+	}
+	
+	/**
+	 * Return a list of child records within this directory.
+	 * 
+	 * @return
+	 */
+	public Record[] listRecords() {
+		// Get all child file records
+		File[] files = this.getFile().listFiles();
+		
+		// Create the list where the records will be stored
+		Record[] list = new Record[files.length];
+		
+		// Loop through all the files
+		for(int i = 0; i < files.length; i++) {
+			
+			// Get a new record from the array
+			Record hold = new Record(files[i].getPath());
+						
+			// If it is a directory
+			if(hold.isDirectory()) {
+				// Add it to the list as a directory
+				list[i] = this.getAsDirectory();
+			}
+			else {
+				// Add the record to the list
+				list[i] = hold;
+			}
+		}
+		
+		// Return the list
+		return list;
 	}
 	
 }
