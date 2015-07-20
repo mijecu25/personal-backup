d2dc48a85a563ab12883d55a375633a430aeabcd
Miguel Velez <miguelvelez@mijecu25.com>
2015-07-20 17:44:10 -0400

    Implemented logic for walking through the file system

--- src/main/java/com/mijecu25/personalbackup/visitors/BackupVisitor.java
+++ src/main/java/com/mijecu25/personalbackup/visitors/BackupVisitor.java
@@ -1,6 +1,7 @@
 package com.mijecu25.personalbackup.visitors;
 
-import java.util.spi.CurrencyNameProvider;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,9 +12,10 @@ import com.mijecu25.personalbackup.file.Path;
 import com.mijecu25.personalbackup.file.Record;
 
 /**
- * @author Miguel Velez
+ * Recursive file tree visitor that backs up records and directories.
  * 
- * @version 0.1.1.2
+ * @author Miguel Velez 
+ * @version 0.1.1.4
  */
 public class BackupVisitor implements Runnable, FileVisitor {
 	
@@ -21,7 +23,8 @@ public class BackupVisitor implements Runnable, FileVisitor {
 	
 	private Directory 		source;
 	private Directory		destination;
-	private LinkedListQueue filesQueue;
+	private LinkedListQueue pathsQueue;
+	private List<Thread> children;
 	private boolean			isDone;
 
 	/**
@@ -30,64 +33,49 @@ public class BackupVisitor implements Runnable, FileVisitor {
 	public BackupVisitor(String source, String destination) {
 		this.source = new Directory(source);
 		this.destination = new Directory(destination);
-		this.filesQueue = new LinkedListQueue();
+		this.pathsQueue = new LinkedListQueue();
+		this.children = new ArrayList<Thread>();
 		this.isDone = false;
 
-		BackupVisitor.logger.info("Created a new BackupManager object");
+		BackupVisitor.logger.info("Created a new BackupVisitor object");
 		BackupVisitor.logger.info("Source: " + this.source);
 		BackupVisitor.logger.info("Destination: " + this.destination);
 		
 		// Add the source directory in the queue
-		this.filesQueue.enqueue(this.source);
+		this.pathsQueue.enqueue(this.source);
 	}
 	
 	/**
-	 * When an object implementing interface Runnable is used to create a 
-	 * thread, starting the thread causes the object's run method to be called 
-	 * in that separately executing thread. 
-	 * <br>
-	 * <br>
-	 * The general contract of the method run is that it may take any action whatsoever.
-	 * <br>
-	 * <br>
 	 * Execute the general logic of backing up files
 	 */
 	@Override
 	public void run() {
-		BackupVisitor.logger.info("Executing backup manager main logic");
+		BackupVisitor.logger.info("Executing Backup Visitor main logic");
 		
 		// Synchronize the backup manager main logic
 		synchronized(this) {
 			
-			// Create holders for records and directories
-			Path currentRecord;
-			Directory currentDirectory;
-			
+			// Create holder for current path
+			Path currentPath;
+									
 			// While there are more files to process
-			while(!this.filesQueue.isEmpty()) {
+			while(!this.pathsQueue.isEmpty()) {
 		
+				try {
 					// dequeue the next file
-					currentRecord = (Path) this.filesQueue.dequeue();
+					currentPath = (Path) this.pathsQueue.dequeue();
 					
-					currentRecord.accept(this);
-										
-//					// If the current record is a directory
-//					if(currentRecord.isDirectory()) {
-//						// Get the current directory
-//						currentDirectory = currentRecord.getAsDirectory();
-//						
-//						Record[] childRecords = (Record[]) currentDirectory.listFiles();
-//						
-//						for(Record child : childRecords) {
-//							this.filesQueue.enqueue(child);
-//						}
-//					}
-//					else {
-//						BackupManager.logger.info("This is where we should copy a file");
-//					}
-//					
+					// Pass this visitor to the current path
+					currentPath.accept(this);
+					
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+															
 			}
-		
+					
 			// Notify a thread waiting for the backup manager that it can continue
 			this.notify();			
 			
@@ -123,17 +111,91 @@ public class BackupVisitor implements Runnable, FileVisitor {
 	public boolean isDone() {
 		return this.isDone;
 	}
-
+	
+	/**
+	 * Visit a record object. Copy the record from the source to the destination.
+	 * 
+	 * @param record
+	 * @return object that is returned after visiting a record.
+	 */
 	@Override
 	public Object visitRecord(Record record) {
-		// TODO Auto-generated method stub
+		BackupVisitor.logger.info(Thread.currentThread().getName() + " - " + record.getPath());
+		if(record.getName().equals("File2.png")) {
+			try {
+				System.out.println("Wiating");
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		// We do not need to return anything
 		return null;
 	}
 
+	/**
+	 * Visit a directory object.
+	 * 
+	 * @param directory
+	 * @return object that is returned after visiting a directory.
+	 */
 	@Override
 	public Object visitDirectory(Directory directory) {
-		// TODO Auto-generated method stub
+		String newDestination = "";
+		
+		// Get the child paths
+		Path[] childPaths = directory.listPaths();
+		
+		// Loop through every child
+		for(Path child : childPaths) {
+//			if(child.equals(this.source)) {
+//				continue;
+//			}
+				
+			// If the child is a record
+			if(child instanceof Record) {
+				// Added to the queue to process
+				this.pathsQueue.enqueue(child);		
+			}
+			// If the child is a directory
+			else if(child instanceof Directory) {	
+				// Get the new destination
+				newDestination = this.getDestinationDirectory(child.getName());
+				
+				// Create a new backup visitor with the child's path and its destination
+				BackupVisitor childVisitor = new BackupVisitor(child.getPath(), newDestination);
+				
+				// Add the thread to the list of children threads of this visitor
+				this.children.add(childVisitor.startAsThread());
+			}
+			
+		}	
+		
+		// Loop through all the threads of this visitor
+		for(Thread thread : this.children) {
+			try {
+				// Wait until this thread dies
+				thread.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		// We do not need to return anything
 		return null;
+	}
+	
+	/**
+	 * Get the destination directory of the source directory.
+	 * 
+	 * @param source directory
+	 * 
+	 * @return string that represents the new destination directory
+	 */
+	private String getDestinationDirectory(String source) {
+		// The destination is the destination of this visitor + the source	
+		return this.destination + "\\" + source;
 	}
 	
 }
